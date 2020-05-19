498fef6714fc8b09533f9fe819c2d237bb7b99af
Matt <mattep96@gmail.com>
2020-05-18 20:54:54 -0400

    Add computer graphics files

---
+++ HW/HW1.py
@@ -0,0 +1,40 @@
+﻿def isPrime(number):
+	for i in range (2, number):
+		if (number % i) == 0:
+			return False
+
+	return True
+
+def findSigmasAndLambdas():
+	lowerBound = input("Please enter a number --> ")
+	upperBound = input("Please enter a larger number --> ")
+	
+	primes = []
+	sigmaPairs = []
+	lambdas = []
+	
+	for i in range(lowerBound, upperBound + 1):
+		if isPrime(i):
+			primes.append(i)
+	
+	if len(primes) > 0:
+		print("Sigma pair(s) in the range (" + str(lowerBound) + "..." + str(upperBound) + ")")
+		for i in range(0, len(primes) - 1):
+			if (primes[i] + primes[i + 1]) % 3 != 0:
+				sigmaPairs.append(primes[i])
+				sigmaPairs.append(primes[i + 1])
+				print(str(primes[i]) + "\t" + str(primes[i + 1]))
+
+		for i in range(0, len(primes)):
+			if sigmaPairs.count(primes[i]) > 1:
+				lambdas.append(primes[i])
+				
+		if len(lambdas) > 0:
+			print("Lambda prime(s) in the range (" + str(lowerBound) + "..." + str(upperBound) + ")")
+			print(lambdas)
+		else:
+			print("No lambda primes found")
+	else:
+		print("No primes found")
+	
+findSigmasAndLambdas()
---
+++ HW/ShowModel.py
@@ -0,0 +1,35 @@
+﻿# Matt Pigliavento
+# Siena College, Fall 2017
+
+import viz
+import vizcam
+
+viz.window.setSize( 640, 480 )
+viz.window.setName( "Model Viewer" )
+
+window = viz.MainWindow
+
+viz.MainWindow.clearcolor( [0,255,0] ) 
+
+viz.eyeheight(0)
+
+viz.add('room.dae')
+
+vizcam.PanoramaNavigate()
+
+vizcam.KeyboardCamera(forward='w',
+                                 backward='s',
+                                 left='q',
+                                 right='e',
+                                 up='r',
+                                 down='f',
+                                 turnRight='d',
+                                 turnLeft='a',
+                                 pitchDown='h',
+                                 pitchUp='y',
+                                 rollRight='j',
+                                 rollLeft='g',
+                                 moveMode=viz.REL_LOCAL,
+                                 moveScale=1.0,
+                                 turnScale=1.0)
+viz.go()
